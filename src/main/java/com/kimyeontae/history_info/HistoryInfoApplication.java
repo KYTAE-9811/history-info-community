0f1a629cbda62c90ead9f99bcc145be01d33df10
kytae <slsl654@naver.com>
2024-10-17 16:44:14 +0900

    초기화 커밋과 푸시 (게시판 템플릿만 추가)

---
+++ src/main/java/com/kimyeontae/history_info/HistoryInfoApplication.java
@@ -0,0 +1,13 @@
+package com.kimyeontae.history_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimyeontae/history_info/HistoryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimyeontae.history_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
